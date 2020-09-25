5b376bf3c5466e9d4f9bea0c672f8a0852469fd8
johnj974 <jjdug@hotmail.com>
2020-09-25 09:45:08 +0000

    changed information display in projects section

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,13 +7,13 @@ function project(name, tech1, tech2, tech3, tech4, description){
     this.description = description;
 }
 
-let metallic = new project("Metallic AA's", "HTML", "CSS", "Bootstrap", "A project about a heavy metal band");
+let metallic = new project("Metallic AA's", "HTML", "CSS", "Bootstrap","Font Awesome", "A project about a heavy metal band");
 let countryInfo = new project("Country Info App", "Javascript", "HTML", "CSS",);
 let triathlon = new project("Triathlon Checklist", "Python", "Flask", "Mongo DB", "CSS");
 let mystery = new project("Mystery Shack", "Python", "Javascript", "Django", "AWS");
 let santa = new project("Santa CV", "HTML", "Javascript", "CSS", "Bootstrap")
 
-
+/*------
 function info(project){
     document.getElementById("projpic").src = "assets/images/candle.jpg";
     document.getElementById("projectname").innerHTML = project.name;
@@ -30,4 +30,74 @@ function noInfo(){
     document.getElementById("projecttech2").innerHTML = null;
     document.getElementById("projecttech3").innerHTML = null;
     document.getElementById("projecttech4").innerHTML = null;
+}
+-------*/
+
+function projectOne(metallic){
+    document.getElementById("metalProject1").innerHTML = metallic.tech1;
+    document.getElementById("metalProject2").innerHTML = metallic.tech2;
+    document.getElementById("metalProject3").innerHTML = metallic.tech3;
+    document.getElementById("metalProject4").innerHTML = metallic.tech4;  
+}
+function clearMetallic(){
+    document.getElementById("metalProject1").innerHTML = null;
+    document.getElementById("metalProject2").innerHTML = null;
+    document.getElementById("metalProject3").innerHTML = null;
+    document.getElementById("metalProject4").innerHTML = null;  
+}
+
+
+function projectTwo(countryInfo){
+    document.getElementById("countryProject1").innerHTML = countryInfo.tech1;
+    document.getElementById("countryProject2").innerHTML = countryInfo.tech2;
+    document.getElementById("countryProject3").innerHTML = countryInfo.tech3;
+    document.getElementById("countryProject4").innerHTML = countryInfo.tech4;  
+}
+function clearCountry(){
+    document.getElementById("countryProject1").innerHTML = null;
+    document.getElementById("countryProject2").innerHTML = null;
+    document.getElementById("countryProject3").innerHTML = null;
+    document.getElementById("countryProject4").innerHTML = null;  
+}
+
+
+function projectThree(triathlon){
+    document.getElementById("triathlonProject1").innerHTML = triathlon.tech1;
+    document.getElementById("triathlonProject2").innerHTML = triathlon.tech2;
+    document.getElementById("triathlonProject3").innerHTML = triathlon.tech3;
+    document.getElementById("triathlonProject4").innerHTML = triathlon.tech4;  
+}
+function clearTriathlon(){
+    document.getElementById("triathlonProject1").innerHTML = null;
+    document.getElementById("triathlonProject2").innerHTML = null;
+    document.getElementById("triathlonProject3").innerHTML = null;
+    document.getElementById("triathlonProject4").innerHTML = null;  
+}
+
+
+function projectFour(mystery){
+    document.getElementById("mysteryProject1").innerHTML = mystery.tech1;
+    document.getElementById("mysteryProject2").innerHTML = mystery.tech2;
+    document.getElementById("mysteryProject3").innerHTML = mystery.tech3;
+    document.getElementById("mysteryProject4").innerHTML = mystery.tech4;  
+}
+function clearMystery(){
+    document.getElementById("mysteryProject1").innerHTML = null;
+    document.getElementById("mysteryProject2").innerHTML = null;
+    document.getElementById("mysteryProject3").innerHTML = null;
+    document.getElementById("mysteryProject4").innerHTML = null;  
+}
+
+
+function projectFive(santa){
+    document.getElementById("santaProject1").innerHTML = santa.tech1;
+    document.getElementById("santaProject2").innerHTML = santa.tech2;
+    document.getElementById("santaProject3").innerHTML = santa.tech3;
+    document.getElementById("santaProject4").innerHTML = santa.tech4;  
+}
+function clearSanta(){
+    document.getElementById("santaProject1").innerHTML = null;
+    document.getElementById("santaProject2").innerHTML = null;
+    document.getElementById("santaProject3").innerHTML = null;
+    document.getElementById("santaProject4").innerHTML = null;  
 }
