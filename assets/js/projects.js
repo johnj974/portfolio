8aba306aa3fea2d194f13bcf1f6b23421c57a685
johnj974 <jjdug@hotmail.com>
2020-09-30 14:04:08 +0000

    tidied code in project section

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -35,74 +35,45 @@ function noInfo(){
 -------*/
 
 function projectOne(metallic){
-    //document.getElementById("metallicTitle").style.color = "rgb(255, 8, 0)";
-    document.getElementById("metalProject1").innerHTML = metallic.tech1;
-    document.getElementById("metalProject2").innerHTML = metallic.tech2;
-    document.getElementById("metalProject3").innerHTML = metallic.tech3;
-    document.getElementById("metalProject4").innerHTML = metallic.tech4; 
-    document.getElementById("metalDescription").innerHTML = `This is a combination of four web pages for a fictional band called The Metallic AA's, The main technologies used were ${metallic.tech1}`; 
+    document.getElementById("metalDescription").innerHTML = `This is a combination of four web pages for a fictional band called The Metallic AA's,
+     The main technologies used were ${metallic.tech1}, ${metallic.tech2}, ${metallic.tech3} and${metallic.tech4}`; 
 }
 function clearMetallic(){
-    //document.getElementById("metallicTitle").style.color = "rgb(206, 21, 181)";
-    document.getElementById("metalProject1").innerHTML = null;
-    document.getElementById("metalProject2").innerHTML = null;
-    document.getElementById("metalProject3").innerHTML = null;
-    document.getElementById("metalProject4").innerHTML = null; 
     document.getElementById("metalDescription").innerHTML = null; 
 }
 
 
 function projectTwo(countryInfo){
-    document.getElementById("countryProject1").innerHTML = countryInfo.tech1;
-    document.getElementById("countryProject2").innerHTML = countryInfo.tech2;
-    document.getElementById("countryProject3").innerHTML = countryInfo.tech3;
-    document.getElementById("countryProject4").innerHTML = countryInfo.tech4;  
+    document.getElementById("countryDescription").innerHTML = `An interactive site that shows information, location and weather for any selected country, 
+    The main technologies used were ${countryInfo.tech1}, ${countryInfo.tech2}, ${countryInfo.tech3}`
 }
 function clearCountry(){
-    document.getElementById("countryProject1").innerHTML = null;
-    document.getElementById("countryProject2").innerHTML = null;
-    document.getElementById("countryProject3").innerHTML = null;
-    document.getElementById("countryProject4").innerHTML = null;  
+    document.getElementById("countryDescription").innerHTML = null;  
 }
 
 
 function projectThree(triathlon){
-    document.getElementById("triathlonProject1").innerHTML = triathlon.tech1;
-    document.getElementById("triathlonProject2").innerHTML = triathlon.tech2;
-    document.getElementById("triathlonProject3").innerHTML = triathlon.tech3;
-    document.getElementById("triathlonProject4").innerHTML = triathlon.tech4;  
+    document.getElementById("triathlonDescription").innerHTML = `A simple to use app that allows users to create, read, update and delete triathlon related information,
+    The main technologies used were ${triathlon.tech1}, ${triathlon.tech2}, ${triathlon.tech3} and ${triathlon.tech4}`
 }
 function clearTriathlon(){
-    document.getElementById("triathlonProject1").innerHTML = null;
-    document.getElementById("triathlonProject2").innerHTML = null;
-    document.getElementById("triathlonProject3").innerHTML = null;
-    document.getElementById("triathlonProject4").innerHTML = null;  
+    document.getElementById("triathlonDescription").innerHTML = null; 
 }
 
 
 function projectFour(mystery){
-    document.getElementById("mysteryProject1").innerHTML = mystery.tech1;
-    document.getElementById("mysteryProject2").innerHTML = mystery.tech2;
-    document.getElementById("mysteryProject3").innerHTML = mystery.tech3;
-    document.getElementById("mysteryProject4").innerHTML = mystery.tech4;  
+    document.getElementById("mysteryDescription").innerHTML = `An early draft for an e-commerce store using the Django framework, 
+    The main technologies used were ${mystery.tech1}, ${mystery.tech2}, ${mystery.tech3} and ${mystery.tech4}` 
 }
 function clearMystery(){
-    document.getElementById("mysteryProject1").innerHTML = null;
-    document.getElementById("mysteryProject2").innerHTML = null;
-    document.getElementById("mysteryProject3").innerHTML = null;
-    document.getElementById("mysteryProject4").innerHTML = null;  
+    document.getElementById("mysteryDescription").innerHTML = null;
 }
 
 
 function projectFive(santa){
-    document.getElementById("santaProject1").innerHTML = santa.tech1;
-    document.getElementById("santaProject2").innerHTML = santa.tech2;
-    document.getElementById("santaProject3").innerHTML = santa.tech3;
-    document.getElementById("santaProject4").innerHTML = santa.tech4;  
+    document.getElementById("santaDescription").innerHTML = `A site showcasing all the skills that Santa has to offer, 
+    The main technologies used were ${santa.tech1}, ${santa.tech2}, ${santa.tech3} and ${santa.tech4}`;  
 }
 function clearSanta(){
-    document.getElementById("santaProject1").innerHTML = null;
-    document.getElementById("santaProject2").innerHTML = null;
-    document.getElementById("santaProject3").innerHTML = null;
-    document.getElementById("santaProject4").innerHTML = null;  
+    document.getElementById("santaDescription").innerHTML = null;  
 }
