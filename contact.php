2da97a0cd0256286eecaf39d913edb0c9a64263d
johnj974 <jjdug@hotmail.com>
2020-10-05 15:54:50 +0000

    changed target email for form, getting a 405 error

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ if (isset($_POST["submit"])){
     $subject = $_POST["subject"];
     $message = $_POST["message"];
 
-    $mailTo = "jjdug@hotmail.com";
+    $mailTo = "jjtester78@gmail.com";
     $headers = "From: ".$eMail;
     $txt = "You have received an e-mail from ".$name."\n\n".$message;
 
